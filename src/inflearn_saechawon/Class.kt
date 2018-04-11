ce84c835b08b0b039e8812a8c9589ab6a3c0dc65
Seongtaek Park <esulove81@gmail.com>
2018-04-11 17:57:28 +0900

    기본 생성자.

---
+++ src/inflearn_saechawon/Class.kt
@@ -0,0 +1,13 @@
+package inflearn_saechawon
+
+fun main(args: Array<String>) {
+    val obj = Customer("코틀린")
+}
+
+// 기본 생성자.
+// 기본 생성자의 파라미터는 init 블럭 안에서만 사용가능.
+class Customer(name: String) {
+    init {
+        print("이름: $name")
+    }
+}
