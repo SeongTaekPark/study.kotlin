3a85f373fcfe46d92b3e44c750df0fb6ea4e78b0
Seongtaek Park <esulove81@gmail.com>
2018-04-12 10:00:01 +0900

    Properties.

---
+++ inflearn_saechawon/src/Properties.kt
@@ -0,0 +1,17 @@
+package src
+
+// Properties
+
+fun main(args: Array<String>) {
+    var obj = Address()
+    println(obj.name)
+}
+
+class Address {
+    var name: String = "Kotlin"
+        // Custom accessors (getter, setter) 재정의.
+        get() {
+            return field + "!!!"
+        }
+        set(value) { field = value }
+}
