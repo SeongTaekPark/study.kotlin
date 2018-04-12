cd1e28dfd0bb3e89f979154b687c1101dc4118e9
Seongtaek Park <esulove81@gmail.com>
2018-04-12 10:28:09 +0900

    Complie time constants

---
+++ inflearn_saechawon/src/ComplieTimeConstants.kt
@@ -0,0 +1,12 @@
+// Compile time constants
+
+// Top level 이거나,
+// Object의 멤버이거나
+// String 이나 프리미티브 타입으로 초기화된 경우.
+const val MY_CONST = "CONST"
+
+@Deprecated(MY_CONST)
+fun main(args: Array<String>) {
+
+}
+
