5151b28deb0aa436d73ce07d2fa8d20fa7b7f5cb
Seongtaek Park <esulove81@gmail.com>
2018-04-18 18:05:14 +0900

    확장 프로퍼티

---
+++ src/kotlin_in_action/chapter_03/3.7.kt
@@ -0,0 +1,19 @@
+package kotlin_in_action.chapter_03
+
+/**
+ * 확장 프로퍼티
+ */
+val String.lastChar: Char
+    get() = get(length - 1)
+var StringBuilder.lastChar: Char
+    get() = get(length - 1)
+    set(value: Char) {
+        this.setCharAt(length - 1, value)
+    }
+
+fun main(args: Array<String>) {
+    println("kotlin".lastChar)
+    val sb = StringBuilder("Kotlin?")
+    sb.lastChar = '!'
+    println(sb)
+}
