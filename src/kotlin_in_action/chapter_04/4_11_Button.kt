35c18b37f9c26cee24d04127834239f315486aac
Seongtaek Park <esulove81@gmail.com>
2018-07-18 17:05:08 +0900

    중첩 클래스

---
+++ src/kotlin_in_action/chapter_04/4_11_Button.kt
@@ -0,0 +1,22 @@
+package ch04.Button1
+
+import java.io.Serializable
+
+interface State: Serializable
+
+interface View {
+    fun getCurrentState(): State
+    fun restoreState(state: State) {}
+}
+
+class Button : View {
+    override fun getCurrentState(): State = ButtonState()
+    override fun restoreState(state: State) {
+        /**/
+    }
+
+    // 이 클래스는 자바의 정적 중첩 클래스와 대응한다.
+    class ButtonState : State {
+        /**/
+    }
+}
