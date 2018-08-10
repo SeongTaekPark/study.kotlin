76bce4efd78fd253f9effe8fb2d402510689c905
Seongtaek Park <esulove81@gmail.com>
2018-08-10 17:36:54 +0900

    클래스 위임 사용하기

---
+++ src/kotlin_in_action/chapter_04/4.22.kt
@@ -0,0 +1,23 @@
+package kotlin_in_action.chapter_04.counting_set
+
+class CountingSet<T>(val innerSet: MutableCollection<T> = HashSet<T>()) :
+        // MutableCollection의 구현을 innerSet에게 위임한다.
+        MutableCollection<T> by innerSet {
+    var objectsAdded = 0
+
+    override fun add(element: T): Boolean {
+        objectsAdded++
+        return innerSet.add(element)
+    }
+
+    override fun addAll(c: Collection<T>): Boolean {
+        objectsAdded += c.size
+        return innerSet.addAll(c)
+    }
+}
+
+fun main(args: Array<String>) {
+    var cset = CountingSet<Int>()
+    cset.addAll(listOf(1, 1, 2))
+    println("${cset.objectsAdded} objects were added, ${cset.size} remain")
+}
